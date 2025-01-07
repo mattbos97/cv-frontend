9116a1ec8d62280f65da96fd0586af93cf5a9d5a
Matt Bos <matt.bos1997@gmail.com>
2025-01-07 16:58:32 +1100

    Added first draft of page content

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,7 +1,17 @@
 export default function Page(){
     return(
-        <main className={"flex flex-1 justify-center items-center"}>
+        <main className={"flex flex-col flex-1 justify-center"}>
             <h1>About me</h1>
+            <p>
+                I am Matt Bos, a 27-year-old software engineer with a passion for innovation and problem-solving.
+                Originally from the Netherlands, I’ve recently relocated to Australia to build a life with my partner.
+                With over three years of experience in software engineering,
+                I specialize in creating innovative solutions that enhance efficiency, reduce costs,
+                and solve complex technical problems.
+                From a young age, I’ve been captivated by technology and its ability to transform ideas into tangible solutions.
+                I approach challenges with a growth mindset, breaking down complex problems into manageable steps.
+                I am eager to collaborate with forward-thinking teams and apply my skills to impactful innovations.
+            </p>
         </main>
     );
 }
