e4fc8d627a13cea0e902fb3a7ef1ca635b6c84ca
Matt Bos <matt.bos1997@gmail.com>
2025-01-07 16:42:13 +1100

    Added basic navigation links

--- app/ui/SideBar.tsx
+++ app/ui/SideBar.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 export default function SideBar(){
+
+    const links = [
+        {name: "Home", href: "/"},
+        {name: "About me", href: "/about-me"},
+        {name: "Experience", href: "/experience"},
+        {name: "Skills", href: "/skills"}
+    ];
+
     return(
-        <nav className={"border-dotted border-black border-2 w-64 flex justify-center"}>
-            <h1>Side bar</h1>
+        <nav className={"border-dotted border-black border-2 w-64 flex items-center flex-col"}>
+            {links.map((link) => {
+                return(
+                    <Link
+                        className={"border-dotted border-black border-2 rounded-full w-48 flex justify-center"}
+                        key={link.name}
+                        href={link.href}
+                    >
+                        {link.name}
+                    </Link>
+                );
+            })}
         </nav>
     );
 }
