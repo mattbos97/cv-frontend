3d99eada585dc65ab9b81cf33f511b37b7d19843
Matt Bos <matt.bos1997@gmail.com>
2025-01-08 19:22:47 +1100

    Resolved

--- app/ui/SideBar.tsx
+++ app/ui/SideBar.tsx
@@ -1,31 +1,17 @@
-'use client'
-import LinkButton from "@/app/ui/LinkButton";
-import {usePathname} from "next/navigation";
+import {LinkButton, LinkButtonParams} from "@/app/ui/LinkButton";
 
-const links = [
-    {name: "Home", href: "/"},
-    {name: "About me", href: "/about-me"},
-    {name: "Experience", href: "/experience"},
-    {name: "Skills", href: "/skills"},
-    {name: "Styles", href: "/styles"}
-];
+const links: LinkButtonParams[] = [
+    {text: "Home", href: "/"},
+    {text: "Personal info", href: "/about-me"},
+    {text: "Experience", href: "/experience"},
+    {text: "Skills", href: "/skills"},
+    {text: "Styles", href: "/styles"}
+]
 
 export default function SideBar(){
-
-    const path = usePathname();
-
     return(
         <nav className={"w-64 flex flex-col items-center space-y-4 p-8 bg-dark-primary"}>
-            {links.map((link) => {
-                return(
-                    <LinkButton
-                        key={link.name}
-                        text={link.name}
-                        href={link.href}
-                        type={path === link.href ? 'secondary' : 'primary'}
-                    />
-                );
-            })}
+            {links.map((link) => <LinkButton key={link.text} parameters={link}/>)}
         </nav>
     );
 }
